d4dc5cdd4ab86eaa56e3fd6f3a21f11a7b0ad35f
doright84 <95347166+doright84@users.noreply.github.com>
2022-01-13 16:32:45 +0900

    화동이 프로세스 수정본2
    위로가기 설정

--- js/script.js
+++ js/script.js
@@ -5,11 +5,19 @@ $(document).ready(function () {
         $('.modal').fadeOut();
     });
     // ---gotop----
+    $(".gotop").hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) { // 400이 넘어가면 고 탑이 슬라이드 다운으로 보이고 아니면 다시 fade out으로 사라짐.
+            $('.gotop').fadeIn();
+        } else {
+            $('.gotop').fadeOut();
+        }
+    });
     $('.gotop').click(function (e) {
         e.preventDefault();
         $('html, body').animate({
             scrollTop: 0
         });
     });
- 
+
 });
